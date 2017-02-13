5e04c5c550982915de4614c421aa8d0db9a23e29
vinvimo <vinvimo@student.42.fr>
2017-02-13 23:28:54 +0100

    brackets

--- brackets.c
+++ brackets.c
@@ -6,7 +6,7 @@
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/07 11:36:35 by exam              #+#    #+#             */
-/*   Updated: 2017/02/08 23:37:58 by vinvimo          ###   ########.fr       */
+/*   Updated: 2017/02/13 23:28:38 by vinvimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,76 +63,30 @@ void	reset_bc(t_struct *v)
 	v->z = 0;
 }
 
-void	brackets_bis(char *argv, t_struct *v, int index)
-{
-	if (argv[index] == '(' || argv[index] == '[' || argv[index] == '{')
-	{
-		if (v->a == 0)
-		{
-			v->a = argv[index];
-			v->aa = get_closing(v->a);
-		}
-		else if (v->a != 0 && v->b == 0 && argv[index] != v->a)
-		{
-			v->b = argv[index];
-			v->bb = get_closing(v->b);
-		}
-		else if (v->a != 0 && v->b != 0 && v->c == 0
-			&& argv[index] != v->a && argv[index] != v->b)
-		{
-			v->c = argv[index];
-			v->cc = get_closing(v->c);
-		}
-	}
-}
-
 int		brackets(char *argv)
 {
 	int			index;
 	t_struct	v;
 
 	index = 0;
-	reset_abc(&v);
-	while (argv[index])
-	{
-		brackets_bis(argv, &v, index);
-		if (argv[index] == v.a)
-			v.x++;
-		if (argv[index] == v.aa)
-			v.x--;
-		else if (v.x < v.y || v.x < v.z)
-		{
-			ft_putstr("Error1\n");
-			return (0);
-		}
-		if (argv[index] == v.b)
-			v.y++;
-		if (argv[index] == v.bb)
-			v.y--;
-		else if (v.y < v.z)
-		{
-			ft_putstr("Error2\n");
-			return (0);
-		}
-		if (argv[index] == v.c)
-			v.z++;
-		if (argv[index] == v.cc)
-			v.z--;
-		if (v.x < 0 || v.y < 0 || v.z < 0)
-		{
-			ft_putstr("Error3\n");
-			return (0);
-		}
-		if (v.y == 0)
-			reset_bc(&v);
-		if (v.x == 0)
-			reset_abc(&v);
-printf("argv[index] = %c\n", argv[index]);
-printf("v.a = %c v.b = %c v.c = %c\nv.aa = %c v.bb = %c v.cc == %c\nv.x = %d v.y = %d v.z = %d\n", v.a, v.b, v.c, v.aa, v.bb, v.cc, v.x, v.y, v.z);
+	while (argv[index]
+	&& (argv[index] != ')' || argv[index] != ']' || argv[index] != '}'))
 		index++;
+	v.a = get_closing(argv[index]);
+	v.aa = argv[index];
+	while (index >= 0 && argv[index] != v.a)
+	{
+		argv[index] = ' ';
+		index--;
 	}
-	if (v.x == 0 && v.y == 0 && v.z == 0)
-		ft_putstr("OK\n");
+	if (index >= 0)
+		argv[index] = ' ';
+	else
+	{
+		ft_putstr("Error\n");
+		return (0);
+	}
+	index++;
 	return (0);
 }
 
